91b73cb56c3ca9bf9e0fd461d51a0b171e2a6b3a
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-05-20 11:37:33 -0400

    DONE

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
+  // initializig variables for grid 
   int intCellWidth = 20;
   int intCellLength = 20;
   int intMargin = 5;
@@ -29,6 +30,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    // setting all values of array to 1
     background(0);
     for (int y = 0; y < intColumnCount; y++){
       for(int x = 0; x < intRowCount; x++){
@@ -41,12 +43,15 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    // drawing squares along the grid with color based on value of their spaces in the array 
     for (int y = 0; y < intColumnCount; y++){
       for(int x = 0; x < intRowCount; x++){
         if (intGrid[y][x] % 2 == 0){
+          // produces green square if value of square is an even number 
           fill(0,255,51);
           rect(intMargin * (x+1) + (intCellWidth * x), intMargin + (intMargin + intCellLength) * y, intCellWidth, intCellLength);
         }else{
+          // produces white square if value of square is an off number 
           fill(255);
           rect(intMargin * (x+1) + (intCellWidth * x), intMargin + (intMargin + intCellLength) * y, intCellWidth, intCellLength);
         }
@@ -55,12 +60,17 @@ public class Sketch extends PApplet {
 	  
   }
 
+  // mouseclicked method to run each time the mouse is clicked 
   public void mouseClicked(){
+    // for loop to run every time mouse is clicked to check where the mouse was clicked, and add 1 to the values of the squares around the square clicked to change its color
     for (int y = 0; y < intColumnCount; y++){
       for(int x = 0; x < intRowCount; x++){
         if (mouseX > intMargin * (x+1) + (intCellWidth * x) && mouseX < ((intMargin * (x+1) + (intCellWidth * x)) + intCellWidth) && mouseY > (intMargin + (intMargin + intCellLength) * y) && mouseY < (intMargin + (intMargin + intCellLength) * y + intCellLength)){
+          // print statement to print coordinates of mouse 
           System.out.println("Mouse Coordinates: (" + (mouseX) + "," + (mouseY) + "); grid coordinates: (row: " + y + " column: " + x + ")");
           System.out.println("");
+
+          // increases values of array based on where on grid the mouse is 
           if (y == 0 && x == 0){
             intGrid[y][x]++;
             intGrid[y+1][x]++;
@@ -110,6 +120,7 @@ public class Sketch extends PApplet {
 
     int Max = 0; 
 
+    // for loop to count how many spaces are green in each row, and also how many continous blocks are green 
     for (int y = 0; y < intColumnCount; y++){
     for(int x = 0; x < intRowCount; x++){
     if (intGrid[y][x] % 2 == 0){
@@ -117,14 +128,19 @@ public class Sketch extends PApplet {
       RowCounter++;
       Continue++;
     }
+
+    // if the continous value of the squares in a row is higher than the current maximum of the row it is replaced
     if (Continue > Max){
       Max = Continue;
     }
+
+    // if the count finds an array vlaue that is odd, the continous value is reset to zero 
     if (intGrid[y][x] % 2 != 0){
       Continue= 0;
     }
   }
 
+  // print statemetns based on how many green cells are present in each data type 
   if (Max <= 2){
     System.out.println("Row " + y + " has " + RowCounter + " cells selected");
     RowCounter = 0;
@@ -139,17 +155,20 @@ public class Sketch extends PApplet {
   }
 }
 
+// for loop to find how many squares are green in each column 
 for(int x = 0; x < intRowCount; x++){
 for (int y = 0; y < intColumnCount; y++){
   if (intGrid[y][x] % 2 == 0){
     ColumnCounter++;
   }
 }
+// print statement based on how many spaces are green in the column 
 System.out.println("Column " + x + " has " + ColumnCounter + " cells selected");
 ColumnCounter = 0;
 }
 
 
+// prints how many cells are selected in total along the entire grid
     System.out.println("");
     System.out.println("Total of " + counter + " cells are selected");
     System.out.println("");
